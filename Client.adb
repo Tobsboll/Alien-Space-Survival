784cf713d3d9cd4dd23f53e36d0008005bf11a46
Tobsboll <tobiasfjellstroem@gmail.com>
2016-04-11 17:00:19 +0200

    Update Client.adb

--- Client.adb
+++ Client.adb
@@ -3,8 +3,47 @@ with Ada.Exceptions;      use Ada.Exceptions;
 with Ada.Text_IO;         use Ada.Text_IO;
 with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 with TJa.Sockets;         use TJa.Sockets;
+with TJa.Keyboard;        use TJa.Keyboard;
+with TJa.Keyboard.Keys;   use TJa.Keyboard.Keys;
 
 procedure Klient is
+   
+   
+   --------------------------------------------------
+   procedure Get_Input(Keyboard_Input : out Key_type) is
+      
+      Input : Boolean;
+      
+   begin
+      Input := True;
+      
+      while Input loop
+	 
+	 Get_Immediate(Keyboard_Input, Input);
+	 
+	 if Is_Up_Arrow(Keyboard_input) or Is_Down_Arrow(Keyboard_input) or Is_Left_Arrow(Keyboard_input) or Is_Right_Arrow(Keyboard_input) or To_Character(Keyboard_input) = ' ' then 
+	    exit;
+	 end if; -- gör att den endast går vidare i koden efter giltig input
+      end loop;
+      
+   end Get_Input;
+   --------------------------------------------------
+   procedure Send_Input(Keyboard_Input : in Key_Type;
+		        Socket         : in Socket_type) is
+      
+   begin
+      
+      if Is_Up_Arrow(Keyboard_input) then Put_Line(Socket, 'w'); 
+      elsif Is_Down_Arrow(Keyboard_input) then Put_Line(Socket, 's');
+      elsif Is_Left_Arrow(Keyboard_input) then Put_Line(Socket, 'a');
+      elsif Is_Right_Arrow(Keyboard_input) then Put_Line(Socket, 'd');
+      elsif To_Character(Keyboard_input) = ' ' then Put_Line(Socket, ' '); 	
+      end if;
+      
+   end Send_Input;
+   --------------------------------------------------
+   
+   
    
    --Socket_type används för att kunna kommunicera med en server
    Socket : Socket_Type;
@@ -13,7 +52,10 @@ procedure Klient is
    NumPlayers : Integer;
    Textlangd : Natural;        --Kommer innehålla längden på denna text
    Resultat  : Natural;        --Resultatet från servern
-
+   Keyboard_Input : Key_Type;
+   -- Input          : Boolean;
+   Esc     : constant Key_Code_Type := 27;
+   
 
 begin
    --Denna rutin kontrollerar att programmet startas med två parametrar.
@@ -68,7 +110,23 @@ begin
    Skip_Line;
    Skip_Line;
       
- 
+   
+   
+   --------------------------------------------------
+   --Game loop.
+   
+   loop
+   
+    Get_Input(Keyboard_input);
+    
+    if To_Key_Code_type(Keyboard_Input) = Key_Esc then
+       exit;
+    end if;
+    
+    Send_Input(Keyboard_Input, Socket);
+    
+   end loop;
+   
    
    --Innan programmet avslutar stängs socketen, detta genererar ett exception
    --hos servern, pss kommer denna klient få ett exception när servern avslutas
@@ -77,3 +135,4 @@ begin
 
 
 end Klient;
+
