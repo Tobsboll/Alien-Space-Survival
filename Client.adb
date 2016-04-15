10828b7576ba32701878d9e9541fd4f5f5246cf6
syte89 <ericandersson2@hotmail.com>
2016-04-15 15:35:03 +0200

    Update Client.adb

--- Client.adb
+++ Client.adb
@@ -1,14 +1,175 @@
-with Ada.Command_Line;    use Ada.Command_Line;
-with Ada.Exceptions;      use Ada.Exceptions;
-with Ada.Text_IO;         use Ada.Text_IO;
-with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
-with TJa.Sockets;         use TJa.Sockets;
-with TJa.Keyboard;        use TJa.Keyboard;
-with TJa.Keyboard.Keys;   use TJa.Keyboard.Keys;
-with Ada.Strings;         use Ada.Strings;
+with Ada.Command_Line;        use Ada.Command_Line;
+with Ada.Exceptions;          use Ada.Exceptions;
+with Ada.Text_IO;             use Ada.Text_IO;
+with Ada.Integer_Text_IO;     use Ada.Integer_Text_IO;
+with tja.window.Elementary;   use tja.window.Elementary;
+with TJa.Sockets;             use TJa.Sockets;
+with TJa.Keyboard;            use TJa.Keyboard;
+with TJa.Keyboard.Keys;       use TJa.Keyboard.Keys;
+with Ada.Strings;             use Ada.Strings;
+with Space_Map;
 
 
 procedure Klient is
+   
+   
+   -------------------------------------------------------------
+   
+   World_X_Length : constant Integer := 62;
+   World_Y_Length : constant Integer := 30;
+   
+   -- Packet som hanterar banan.
+   package Bana is
+      new Space_map(X_Width => World_X_Length,
+		    Y_Height => World_Y_Length);
+   use Bana;
+   
+   --------------------------------------------------------------
+   
+   --  type X_Led is array(1 .. World_X_Length) of Character;
+   --  type World is array(1 .. World_Y_Length) of X_Led;
+   
+   type XY_Type is array(1 .. 2) of Integer;
+   type Shot_Type is array (1 .. 5) of XY_Type;
+   
+   type Ship_spec is 
+      record
+  	 XY      : XY_Type; 
+  	 Lives   : Integer; 
+  	 S       : Shot_Type;
+      end record;
+   
+   type Enemy_Ship_Spec is
+      record
+	 XY                 : XY_Type;
+	 Lives              : Integer;
+	 Shot               : Shot_Type;
+	 Movement_Selector  : Integer; -- så att jag kan hålla koll på vad varje skepp har för rörelsemönster
+	                               --, då kan vi ha olika typer av fiender på skärmen samtidigt.
+	 Direction_Selector : Integer; -- kanske inte behövs, men håller i nuläget koll på om skeppet är på väg
+	                               -- åt höger eller vänster.
+	 Active             : Boolean;
+      end record;
+   
+   
+   type Player_Type is
+      record
+  	 Playing    : Boolean;
+  	 Name       : String(1..24);
+  	 NameLength : Integer;
+  	 Ship       : Ship_Spec;
+  	 Score      : Integer;
+      end record;
+   
+   type Player_Array is array (1..4) of Player_Type;
+   
+   type Enemies is array (1 .. 50) of Enemy_Ship_Spec;
+   
+   type Game_Data is
+      record
+  	 Layout   : Bana.World;        -- Banan är i packetet så att både klienten och servern 
+	                               -- hanterar samma datatyp / Eric
+  	 Players   : Player_Array;     -- Underlättade informationsöverföringen mellan klient och server.
+  	 Wave     : Enemies;
+      end record; 
+   -------------------------------------------------------------
+   
+   
+   -- Tar emot datan som skickas från servern till klienten   
+   --------------------------------------------------------------
+   procedure Get_Game_Data(Socket : Socket_Type;
+		      Data : out Game_Data) is
+      
+      
+      -- Tar emot spelarens skeppdata    
+      -------------------------------------------------
+      procedure Get_Ship_Data(Socket : in Socket_Type;
+			      Ship   : out Ship_Spec) is
+      begin
+	 
+	 Get(Socket,Ship.XY(1));
+	 Get(Socket,Ship.XY(2));
+	 Get(Socket,Ship.Lives);
+	 
+	 for I in Shot_Type'Range loop
+	    Get(Socket,Ship.S(I)(1));
+	    Get(Socket,Ship.S(I)(2));
+	 end loop;
+	 
+      end Get_Ship_Data;
+      -------------------------------------------------
+      
+      Player_Playing : Integer;
+      Enemies_Active : Integer;
+      
+   begin
+      -- Tar emot Banan.
+      for I in World'Range loop
+	 for J in X_Led'Range loop
+	    Get(Socket, Data.Layout(I)(J));
+	 end loop;
+      end loop;
+      
+      
+      -- Tar emot spelarnas Information
+      for I in Player_Array'Range loop
+	 -- Tar emot om spelaren spelar.
+	 Get(Socket,Player_Playing);
+	 if Player_Playing = 1 then
+	    Data.Players(I).Playing := True;
+	    
+	    
+	    -- Tar emot spelarens namn och namnlängd.
+	    Get_line(Socket,Data.Players(I).Name, Data.Players(I).NameLength );
+	    
+	    
+	    -- Tar emot spelarens skeppdata
+	    Get_Ship_Data(Socket,Data.Players(I).Ship);
+	    
+	    
+	    -- Tar emot spelarens poäng
+	    Get(Socket,Data.Players(I).Score);
+	    
+	    
+	    -- Tar inte emot mer data om inte spelaren spelar.
+	 elsif Player_Playing = 0 then
+	    Data.Players(I).Playing := False;
+	 end if;
+    
+      end loop;	 
+      
+      -- Tar emot Fiende vågen.
+      for I in Enemies'Range loop
+	 Get(Socket,Enemies_Active);
+	 if Enemies_Active = 1 then
+	    
+	    Data.Wave(I).Active := True;
+	    
+	    Get(Socket,Data.Wave(I).XY(1));
+	    Get(Socket,Data.Wave(I).XY(2));
+	    
+	    Get(Socket,Data.Wave(I).Lives);    -- Kanske inte behövs skicka/ta emot
+	    
+	    
+	    for J in Shot_Type'Range loop
+	       Get(Socket,Data.Wave(I).Shot(J)(1));
+	       Get(Socket,Data.Wave(I).Shot(J)(2));
+	    end loop;
+	    
+	    Get(Socket, Data.Wave(I).Movement_Selector);    -- Kanske inte behövs skicka/ta emot
+	    Get(Socket, Data.Wave(I).Direction_Selector);    -- Kanske inte behövs skicka/ta emot
+	    
+	    
+	    -- Tar inte emot mer data om Fienden inte är aktiv.
+	 elsif Enemies_Active = 0 then
+	    Data.Wave(I).Active := False;
+	 end if;
+      end loop;
+
+      
+      
+   end Get_Game_Data;
+   ---------------------------------------------------------------
    
    
    --------------------------------------------------
@@ -77,6 +238,8 @@ procedure Klient is
    Keyboard_Input : Key_Type;
    -- Input          : Boolean;
    Esc     : constant Key_Code_Type := 27;
+   Data         : Game_Data;    -- Innehåller all spelinformation som tas emot från servern.
+   Loop_Counter : Integer;      -- Innehåller Serverns loopar (Kanske kan kontrollera syncningen lite mer)
    
 
 begin
@@ -138,8 +301,12 @@ begin
   
   
    loop
+      Get(Socket,Loop_Counter);    -- tar emot serverns loop_counter
+      Clear_Window;
+      -- Hämtar all data från servern
+      Get_Game_Data(Socket,Data);
       
-      -- put world // Andreas
+      Put_World(Data.Layout,1,1);  -- put world // Eric
       -- put Ships // Andreas
       
     Get_Input(Keyboard_input);
