0123960f0516af1ae74706cc25839cc67ad3159d
Tobsboll <tobiasfjellstroem@gmail.com>
2016-04-26 00:39:53 +0200

    Update enemy_ship_handling.ads

--- enemy_ship_handling.ads
+++ enemy_ship_handling.ads
@@ -2,6 +2,8 @@ with Definitions;               use Definitions;
 with TJa.Sockets;               use TJa.Sockets;
 with Ada.Text_IO;               use Ada.Text_IO;
 with Ada.Integer_Text_IO;       use Ada.Integer_Text_IO;
+with Object_Handling;           use Object_Handling;
+with Ada.Numerics.Discrete_Random;
 with Ada.Unchecked_Deallocation;
 
 
@@ -32,13 +34,38 @@ package Enemy_Ship_Handling is
 	 Next              : Enemy_List;
       end record;
    
+   type Enemy_List_Array is
+     array (1..4) of Enemy_List;
+   
+   
+   
+   --------------------------------------------------
+   --Randomgenerator för fiendeskeppens skott
+   --------------------------------------------------
+   subtype One_To_Twenty is Integer range 1..20;
+   
+   
+   package One_To_Twenty_Random is
+      new Ada.Numerics.Discrete_Random(Result_Subtype => One_To_Twenty);
+   use One_To_Twenty_Random;
+   
+ --  Chance_For_Shot : Generator;
+   
    --------------------------------------------------
    -- PROCEDURES AND FUNCTIONS
    --------------------------------------------------
    
    procedure Spawn_ship(Enemy_Type, X, Y, Difficulty, Num_Lives, Direction, Movement_Type : in Integer;
 			Enemies_List : in out Enemy_List);
-   procedure Update_Enemy_Position(Enemies : in out Enemy_List);
+   procedure Spawn_Wave(Num_To_Spawn  : in out Integer;
+			Enemy_Type    : in Integer;
+			Movement_Type : in Integer;
+			Direction     : in Integer;
+			Enemies_List  : in out Enemy_List);
+   procedure Update_Enemy_Position(Enemies : in out Enemy_List;
+				   Shot_List : in out Object_List);
+   function Last_List(All_Enemies : in Enemy_List_Array) return Integer;
+   function At_Lower_limit(Enemies : in Enemy_List) return Boolean;
    function Next_To_Wall(Enemies : in Enemy_List) return Boolean;
    procedure Move_To_Side(Enemies : in out Enemy_List);
    procedure Change_Direction(Enemies : in out Enemy_List);
@@ -48,10 +75,20 @@ package Enemy_Ship_Handling is
 			     Socket  : in Socket_Type);
    procedure Get_Enemy_Ships(Enemies : in out Enemy_List;
 			     Socket  : in Socket_Type);
+   procedure Create_Enemy_Shot(Enemy_Type, X, Y : in Integer;
+   			       Shot_List : in out Object_List);
+   procedure Shot_Generator(Enemies : in out Enemy_List;
+   			    Chance_For_Shot : in out Generator;
+   			      Shot_List : in out Object_List);
+   
    
    
    procedure Free is
       new Ada.Unchecked_Deallocation(Enemy_Ship_Type, Enemy_List);
+
+   
+
+   
    
    
       
