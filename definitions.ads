ce5385136f43186ae4c07746d884b30ac1cdd023
syte89 <ericandersson2@hotmail.com>
2016-05-01 21:10:23 +0200

    Update definitions.ads

--- definitions.ads
+++ definitions.ads
@@ -4,7 +4,7 @@ with TJa.Sockets;                  use TJa.Sockets;
 package Definitions is
    
    World_X_Length : constant Integer := 110;
-   World_Y_Length : constant Integer := 30;
+   World_Y_Length : constant Integer := 35;
    
    type X_Led is array(1 .. World_X_Length) of Character;
    type World is array(1 .. World_Y_Length) of X_Led;
@@ -80,7 +80,7 @@ package Definitions is
    
       type Game_Data is
       record
-   	 Layout   : World;             -- Banan är i packetet så att både klienten och servern 
+   	 Map      : World;             -- Banan är i packetet så att både klienten och servern 
    	                               -- hanterar samma datatyp / Eric
    	 Players  : Player_Array;      -- Underlättade informationsöverföringen mellan klient och server.
 	 
@@ -91,16 +91,16 @@ package Definitions is
    ---------------------------------------------------
    --| X,Y Koordinater för alla fönster
    ---------------------------------------------------
-   Gameborder_X : constant Integer := 1; 
-   Gameborder_Y : constant Integer := 1;
+   Gameborder_X : constant Integer := 5; 
+   Gameborder_Y : constant Integer := 3;
    
-   World_Box_X      : constant Integer := Gameborder_X;
+   World_Box_X      : constant Integer := Gameborder_X-1;
    World_Box_Y      : constant Integer := Gameborder_Y;
    World_Box_Length : constant Integer := World_X_Length;
    World_Box_Heigth : constant Integer := World_Y_Length;
    
-   Highscore_Window_X      : constant Integer := Gameborder_X+world_X_Length;
-   Highscore_Window_Y      : constant Integer := Gameborder_Y;
+   Highscore_Window_X      : constant Integer := Gameborder_X+World_X_Length;
+   Highscore_Window_Y      : constant Integer := Gameborder_Y+1;
    Highscore_Window_Width  : constant Integer := 33;
    Highscore_Window_Height : constant Integer := 2;
    
@@ -116,8 +116,13 @@ package Definitions is
    ---------------------------------------------------
    Background_Colour_1   : constant Colour_Type := Black;     -- Terminalen
    Text_Colour_1         : constant Colour_Type := White;     -- Terminalen
-   Background_Colour_2   : constant Colour_Type := Black;     -- Scorewindow
-   Text_Colour_2         : constant Colour_Type := White;     -- Scorewindow
+   HighScore_Background  : constant Colour_Type := Black;     -- Scorewindow
+   HighScore_Border      : constant Colour_Type := Dark_Grey; -- Scorewindow
+   Chatt_Background      : constant Colour_Type := Black;     -- Chatt
+   Chatt_Border          : constant Colour_Type := Dark_Grey; -- Chatt
+   Game_Wall_Background  : constant Colour_Type := Magenta;       -- Gameworld
+   Game_Wall_Line        : constant Colour_Type := Bright_Magenta;-- Gameworld
+   Game_Wall_Worm        : constant Colour_Type := Black;     -- Gameworld
    Menu_Background       : constant Colour_Type := Dark_Grey; -- Startmenu
    Menu_Selected         : constant Colour_Type := Blue;      -- Startmenu
    Menu_Non_Selected     : constant Colour_Type := Green;     -- Startmenu
@@ -125,6 +130,8 @@ package Definitions is
    Nickname_Write_Box    : constant Colour_Type := Blue;      -- Enter Nickname
    Alien_Body            : constant Colour_Type := Green;     -- Background
    Alien_Eyes            : constant Colour_Type := Yellow;    -- Background
+   Top_BG_Colour         : constant Colour_Type := Bright_Magenta; -- Game Terminal
+   Bottom_BG_Colour      : constant Colour_Type := Bright_Magenta; -- Game Terminal
    
    Enemy_Laser_1         : constant Colour_Type := Bright_Green;
    Player_Laser_1        : constant Colour_Type := Bright_Red;
@@ -139,7 +146,13 @@ package Definitions is
    Background_Battle_Smallship       : constant Boolean := True;
    Background_Battle_Earth           : constant Boolean := True;
    Background_Battle_Alien           : constant Boolean := False;
+   Wall_Worm                         : constant Boolean := True;
    ---------------------------------------------------
+   
+   
+   -- Justerar bakgrunden automatiskt efter vilka längder man väljer. (Bara för test)
+   Auto_Background                   : constant Boolean := False;
+   Background                        : constant Boolean := True;
    
    
 end Definitions;
