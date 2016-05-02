ed1da7a0b0c1ab9468c63ca2c1b7e859b54405e7
syte89 <ericandersson2@hotmail.com>
2016-05-02 12:26:22 +0200

    Update and rename menu.adb to menu_handling.adb

--- menu_handling.adb
+++ menu_handling.adb
@@ -1,4 +1,6 @@
-package body Menu is
+with Box_Hantering;           use Box_Hantering;
+
+package body Menu_Handling is
    
    procedure Put_Menu(Choice        : in out Character;
 		      NumPlayers    : in out Integer;
@@ -8,13 +10,56 @@ package body Menu is
 		      Player_Name_Length : in out Integer
 		     ) is
       
-      Position_X : Integer := Gameborder_X;
-      Position_Y : Integer := Gameborder_Y;
+      
+      procedure Choose_Nickname_Window(X, Y : in Integer) is
+	 
+      begin
+	 
+	 Set_Colours(Black, Menu_Background);
+	 Put_Block_Box(X, Y, 15, 5, Menu_Background, Nickname_Border_Box);
+	 
+	 
+	 Goto_XY(X+1, Y+1);
+	 Put(" Nickname    ");
+	 Goto_XY(X+2, Y+3);
+	 Put("           ");
+	 
+	 Put_Box(X+1, Y+2, 13, 2, Menu_Background, Nickname_Write_Box);
+	 Goto_XY(X+2, Y+3);
+	 
+	 
+      end Choose_Nickname_Window;
+      
+      
+      procedure Menu(Option : in Integer;
+		     Mark   : in Integer;
+		     X      : in Integer;
+		     Y      : in Integer;
+		     Width  : in Integer;
+		     Height : in Integer;
+		     Name   : in String) is
+	 
+      begin
+	 Set_Colours(Menu_Text,Menu_Background);
+	 
+	 if Option = Mark then
+	    Put_Block_Box(X, Y, Width, Height, Menu_Background, Menu_Selected);	 
+	 else
+	    Put_Block_Box(X, Y, Width, Height, Menu_Background, Menu_Non_Selected);
+	 end if;
+	 
+	 Goto_XY(X+1, Y+1);
+	 Put(Name);
+      end Menu;
+      
+      
+      Position_X : Integer := Gameborder_X+(World_X_Length/2)-6;
+      Position_Y : Integer := Gameborder_Y + 10;
       Max_Length : Integer := 10;
       
    begin
-      if Choice = 'S' then          -- At start enters nickname
-	 Choose_Nickname_Window;    -- A window for entering nickname
+      if Choice = '0' then          -- At start enters nickname
+	 Choose_Nickname_Window(Position_X-1, Position_Y+5);    -- A window for entering nickname
 	 Cursor_Visible;
 	 Get_String(Player_Name, Player_Name_Length, Max_Length,  -- procedure that gets and put what you write
 		    (World_X_Length/2-5)+Gameborder_X, Gameborder_Y+18, White, Menu_Background);
@@ -24,7 +69,12 @@ package body Menu is
       end if;
       
       if  Choice = '1' then                -- Startmenu
-      	 Menu_Start(Option, Position_X, Position_Y);
+      	 
+	 Menu(Option, 1, Position_X, Position_Y   , 13, 2, "   Start   ");
+	 Menu(Option, 2, Position_X, Position_Y+4 , 13, 2, "Multiplayer");
+	 Menu(Option, 3, Position_X, Position_Y+8 , 13, 2, " Settings  ");
+	 Menu(Option, 4, Position_X, Position_Y+12, 13, 2, "  Credits  ");
+	 Menu(Option, 5, Position_X, Position_Y+16, 13, 2, "   Exit    ");
 	 
 	 if Is_Down_Arrow(Navigate_Input) then          -- Down
 	    if Option = 5 then
@@ -43,7 +93,8 @@ package body Menu is
 	       NumPlayers := 1;
 	       Choice := 'C';
 	    elsif Option = 2 then               -- Multiplayer
-	       Choice := '2';
+	       Choice := '2';	    
+	       Option := 2;
 	    elsif Option = 3 then               -- Settings
 	       null;
 	    elsif Option = 4 then               -- Credits
@@ -51,11 +102,14 @@ package body Menu is
 	    elsif Option = 5 then               -- Exit
 	       Choice := 'E';
 	    end if;
-	    Option := 1;
 	 end if;
 	 
-      elsif Choice = '2' then           -- Multiplayer menyn
-	 Menu_Multiplayer(Option, Position_X, Position_Y);
+      elsif Choice = '2' then           -- Multiplayer menu
+	 
+	 Menu(Option, 1, Position_X, Position_Y  , 13, 2, "   Join    ");
+	 Menu(Option, 2, Position_X, Position_Y+4, 13, 2, "  Create   ");
+	 Menu(Option, 3, Position_X, Position_Y+8, 13, 2, "   Back    ");
+	 
 	 
 	 if Is_Down_Arrow(Navigate_Input) then        -- Up
 	    if Option = 3 then
@@ -75,7 +129,7 @@ package body Menu is
 	       NumPlayers := 0;
 	       Choice := 'J';
 	    elsif Option = 2 then     -- Create Multiplayer
-	       Choice := 'M';
+	       Choice := '3';
 	       Option := 2;
 	    elsif Option = 3 then     -- Back
 	       Choice := '1';
@@ -83,9 +137,14 @@ package body Menu is
 	    end if;
 	 end if;
 	 
-      elsif Choice = 'M' then                   -- Create Game window
-	 Multiplayer_Create(Option,Position_X, Position_Y);
-	  
+      elsif Choice = '3' then                   -- Create Game window
+	 
+	 Menu(Option, 1, Position_X-15, Position_Y+4, 13, 2, " 2 Players ");
+	 Menu(Option, 2, Position_X   , Position_Y+4, 13, 2, " 3 Players ");
+	 Menu(Option, 3, Position_X+15, Position_Y+4, 13, 2, " 4 Players ");
+	 Menu(Option, 4, Position_X   , Position_Y+8, 13, 2, "   Back    ");
+	 
+	 
 	 if Is_Down_Arrow(Navigate_Input) then        -- Down
 	    if Option = 4 then
 	       Option := 2;
@@ -111,7 +170,7 @@ package body Menu is
 	       end if;	    
 	    else
 	       Choice := '2';
-	       Option := 1;
+	       Option := 3;
 	    end if;
 	 elsif Is_Left_Arrow(Navigate_Input) then  -- Left
 	    if Option in 2..3 then
@@ -135,4 +194,4 @@ package body Menu is
    
    end Put_Menu;
    
-end Menu;
+end Menu_Handling;
