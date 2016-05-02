4e52a9d5d6b612fa29668d1553448c5d5d3e468a
syte89 <ericandersson2@hotmail.com>
2016-05-02 12:24:43 +0200

    Update and rename menu.ads to menu_handling.ads

---
+++ menu_handling.ads
@@ -0,0 +1,28 @@
+with Ada.Text_IO;       use Ada.Text_IO;
+with TJa.Keyboard;      use TJa.Keyboard;
+with TJa.Keyboard.Keys; use TJa.Keyboard.Keys;
+with TJa.Window.Text;   use TJa.Window.Text;
+with Definitions;       use Definitions;
+with Input;             use Input;
+with Window_Handling;   use Window_Handling;
+
+
+package Menu_Handling is
+   
+   Menu_Text       : Colour_Type := Black;
+   Menu_Background : Colour_Type := Dark_Grey;
+   Option          : Integer := 1;   -- Saves where you are in the menu
+   Navigate_Input  : Key_Type;       -- Players navigate input
+   
+   procedure Put_Menu(Choice     : in out Character; -- 0 = Choose Nickname
+		                                     -- 1 = Start Menu
+		                                     -- 2 = Multiplayer Menu
+		                                     -- 3 = Create Game Window
+		      NumPlayers : in out Integer;
+		      Portadress : in out Integer;
+		      Ipadress      : in out String; 
+		      Player_Name   : in out String; 
+		      Player_Name_Length : in out Integer
+		     );
+		  
+end Menu_Handling;
