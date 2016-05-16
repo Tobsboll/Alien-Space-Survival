bd192e1f605f13b515f63ec13e97c6fab9ab08c0
syte89 <ericandersson2@hotmail.com>
2016-05-16 17:17:03 +0200

    Update window_handling.ads

--- window_handling.ads
+++ window_handling.ads
@@ -1,6 +1,37 @@
+with TJa.Window.Text;         use TJa.Window.Text;
+with Ada.Text_IO;             use Ada.Text_IO;
+
+
 package Window_Handling is
    
    type Echo_Type is (On, Off);
+   
+   procedure Put_Box(X           : in Integer;
+		     Y           : in Integer;
+		     Width       : in Integer;
+		     Height      : in Integer;
+		     Background  : in Colour_Type;
+		     Text_Colour : in Colour_Type);
+   
+   procedure Put_Double_Line_Box(X           : in Integer;
+				 Y           : in Integer;
+				 Width       : in Integer;
+				 Height      : in Integer;
+				 Background  : in Colour_Type;
+				 Text_Colour : in Colour_Type);
+   
+   procedure Put_Block_Box(X           : in Integer;
+			   Y           : in Integer;
+			   Width       : in Integer;
+			   Height      : in Integer;
+			   Background  : in Colour_Type;
+			   Text_Colour : in Colour_Type);
+   
+   procedure Put_Space_Box(X           : in Integer;
+			   Y           : in Integer;
+			   Width       : in Integer; 
+			   Height      : in Integer;
+			   Background  : in Colour_Type);
    
    procedure Clear_Window;
    
@@ -8,7 +39,7 @@ package Window_Handling is
    
    procedure Set_Echo (Echo : in Echo_Type);
    
-   procedure Set_Window_Title (Name : in String;
+   procedure Set_Window_Title (Name   : in String;
 			       Number : in Integer := 0);
    
    procedure Cursor_Visible;
