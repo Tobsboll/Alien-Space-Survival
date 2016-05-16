4d6ffd044e3df14d196c373a54a51e860c9cb4ce
syte89 <ericandersson2@hotmail.com>
2016-05-16 17:26:10 +0200

    Create task_printer.ads

---
+++ task_printer.ads
@@ -0,0 +1,63 @@
+with Map_Handling;        use Map_Handling;
+with Definitions;         use Definitions;
+with TJa.Window.Text;     use TJa.Window.Text;
+with Enemy_ship_handling; use Enemy_Ship_handling;
+with Object_Handling;     use Object_Handling;
+with Player_Handling;     use Player_Handling;
+with Window_Handling;     use Window_Handling;
+with Background_Handling; use Background_Handling;
+with Menu_Handling;       use Menu_Handling;
+with Ada.Text_IO;         use Ada.Text_IO;
+with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
+with Graphics;            use Graphics;
+
+package Task_Printer is
+   
+   
+   task Print_All is
+      entry Print_Everything(D  : in Game_Data;
+			     W  : in Enemy_List_array;
+			     A  : in Object_List;
+			     S  : in Object_List;
+			     O  : in Object_List;
+			     P  : in Object_List;
+			     N  : in Integer;
+			     G  : in Integer;
+			     K  : in Integer;
+			     C  : in Character); 
+      entry Get_Choice(C : out Character);
+   end Print_All;
+   
+   procedure Printer(D  : in Game_Data;
+		     W  : in Enemy_List_array;
+		     A  : in Object_List;
+		     S  : in Object_List;
+		     O  : in Object_List;
+		     P  : in Object_List;
+		     N  : in Integer;
+		     G  : in Integer;
+		     K  : in Integer;
+		     C  : in Character) renames Print_All.Print_Everything;
+   
+   procedure Get_From_Printer(C : out Character) renames Print_All.Get_Choice;
+   
+   task Astroid is
+      entry Print(L : Object_List);
+   end Astroid;
+   
+   task Shot is
+      entry Print(L:Object_List);
+   end Shot;
+   
+   task Obstacle is
+      entry Print(L:Object_List);
+   end Obstacle;
+   
+   task Powerup is
+      entry Print(L:Object_List);
+   end Powerup; 
+   
+   
+   
+   
+end Task_Printer;
