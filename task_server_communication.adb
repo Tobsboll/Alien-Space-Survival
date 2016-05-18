4ee4d9ff901e2d72af42403dc41ca2f93e09e89f
syte89 <ericandersson2@hotmail.com>
2016-05-18 11:38:13 +0200

    Update task_server_communication.adb

--- task_server_communication.adb
+++ task_server_communication.adb
@@ -9,38 +9,39 @@ package body Task_Server_Communication is
       
    begin
       loop
-	 accept Send_Data(Socket        : in Socket_Type;
-			  Astroid_List  : in Object_List;
-			  Shot_List     : in Object_List;
-			  Obstacle_List : in Object_List;
-			  Powerup_List  : in Object_List;
-			  Game          : in Game_Data;
-			  Waves         : in Enemy_List_Array) do
+	 select
+	    accept Send_Data(Socket        : in Socket_Type;
+			     Astroid_List  : in Object_List;
+			     Shot_List     : in Object_List;
+			     Obstacle_List : in Object_List;
+			     Powerup_List  : in Object_List;
+			     Game          : in Game_Data;
+			     Waves         : in Enemy_List_Array) do
+	       
+	       Soc := Socket;
+	       A   := Astroid_List;
+	       S   := Shot_List;
+	       O   := Obstacle_List;
+	       P   := Powerup_List;
+	       G   := Game;
+	       W   := Waves;
+	       
+	    end Send_Data;
 	    
-	    Soc := Socket;
-	    A   := Astroid_List;
-	    S   := Shot_List;
-	    O   := Obstacle_List;
-	    P   := Powerup_List;
-	    G   := Game;
-	    W   := Waves;
+	    Put(Soc, A);
+	    Put(Soc, S);
+	    Put(Soc, O);
+	    Put(Soc, P);
+	    Send_Map(Soc, G);      -- Map_Handling
+	    Put_Game_Data(Soc, G);
 	    
-	 end Send_Data;
-	 
-	 Put(Soc, A);
-	 Put(Soc, S);
-	 Put(Soc, O);
-	 Put(Soc, P);
-	 Send_Map(Soc, G);      -- Map_Handling
-	 Put_Game_Data(Soc, G);
-	 
-	 for J in W'Range loop
-	    Put_Enemy_ships(W(J), Soc);
-	 end loop;      
-	 
-	 -- Game Over Check
-	 Put_line(Soc, G.Settings.Gameover);
-	 
+	    for J in W'Range loop
+	       Put(Soc, W(J));
+	    end loop;      
+	    
+	    -- Game Over Check
+	    Put_line(Soc, G.Settings.Gameover);
+	 end select;	 
       end loop;
    end Send_Data_To_Player;
    
