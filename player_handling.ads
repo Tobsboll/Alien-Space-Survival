4e829f796271b73e489da34704a4be172127257f
syte89 <ericandersson2@hotmail.com>
2016-05-18 11:34:34 +0200

    Update player_handling.ads

--- player_handling.ads
+++ player_handling.ads
@@ -8,6 +8,7 @@ with TJa.Window.Text;         use TJa.Window.Text;
 with Window_Handling;         use Window_Handling;
 with Definitions;             use Definitions;
 with Graphics;                use Graphics;
+with Score_Handling;          use Score_Handling;
 
 package Player_Handling is
    
@@ -15,20 +16,6 @@ package Player_Handling is
    Player_Colour_Length : Integer;
    Keyboard_Input       : Key_Type;
    
-   procedure Get_Input;
-   
-   procedure Get_Input(Key_Board_Input : out Key_Type);
-   
-   procedure Send_Input(Socket : in Socket_type);
-   
-   function Is_Esc return Boolean;
-   
-   procedure Get_String(Text        : out String;
-			Text_Length : out Integer;
-			Max_Text    : in Integer;
-			X, Y        : in Integer;
-		        Text_Col    : in Colour_Type;
-			Back_Col    : in Colour_Type);
    
    ----------------------------------------------------------
    ----------------------------------------------| Server |--
@@ -41,7 +28,7 @@ package Player_Handling is
    procedure Remove_Player(Socket     : in out Socket_Type; 
 			   Player_Num : in Integer);
    procedure Sort_Scoreboard(Game : in out Game_Data;
-			    Num_Players : in Integer);
+			     Num_Players : in Integer);
    procedure Add_All_Players(Listener : in Listener_Type;
 			     Sockets : in out Socket_Array;
 			     Num_Players : out Integer);
@@ -49,10 +36,27 @@ package Player_Handling is
 				     Player : in out Player_Type);
    procedure Send_Players_Nick_Colour(Socket : in Socket_Type;
 				      Player : in Player_Type);
+   function Check_Players_Choice(Players     : in Players_Choice_Array;
+				 C           : in Character;
+				 Num_Players : in Integer) return Boolean;
+   procedure Get_Players_Choice( Players_Choice : in out Players_Choice_Array;
+				 Sockets        : in Socket_Array;
+				 Num_Players     : in Integer;
+				 Game           : in Game_Data);
    
    ----------------------------------------------------------
    ----------------------------------------------| Client |--
    ---------------------------------------------------------- 
+   procedure Get_Input;
+   procedure Get_Input(Key_Board_Input : out Key_Type);
+   procedure Send_Input(Socket : in Socket_type);
+   function Is_Esc return Boolean;
+   procedure Get_String(Text        : out String;
+			Text_Length : out Integer;
+			Max_Text    : in Integer;
+			X, Y        : in Integer;
+		        Text_Col    : in Colour_Type;
+			Back_Col    : in Colour_Type);
    procedure Get_Game_Data(Socket : Socket_Type;
 			   Data : out Game_Data);
    procedure Put_Player_Ships (Data : in  Game_Data;
@@ -74,11 +78,13 @@ package Player_Handling is
 				     Klient_Number : in Integer);
    procedure Get_Player_Nick_Colour(Socket : in Socket_Type;
 				    Player : in out Player_Type);
-   function Check_Players_Choice(Players     : in Players_Choice_Array;
-				 C           : in Character;
-				 Num_Players : in Integer) return Boolean;
-   procedure Get_Players_Choice( Players_Choice : in out Players_Choice_Array;
-				 Sockets        : in Socket_Array;
-				 Num_Players     : in Integer);
    
+   
+   ---------------------------------------------------------- 
+   -------------------------------------| Server & Client |--
+   ---------------------------------------------------------- 
+   procedure Send_Players_Choice( Players_Choice : in out Players_Choice_Array;
+				   Sockets        : in Socket_Array;
+				   Num_Players    : in Integer);
+     
 end Player_Handling;
