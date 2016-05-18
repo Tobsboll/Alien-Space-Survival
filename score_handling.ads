a921c7e513eb3a96578a92038ca8b5812c3c50cc
syte89 <ericandersson2@hotmail.com>
2016-05-18 17:18:59 +0200

    Update score_handling.ads

--- score_handling.ads
+++ score_handling.ads
@@ -2,6 +2,8 @@ with Definitions;           use Definitions;
 with Ada.Text_IO;           use Ada.Text_IO;
 with Ada.Integer_Text_IO;   use Ada.Integer_Text_IO;
 with Window_Handling;       use Window_Handling;
+with TJa.Sockets;           use TJa.Sockets;
+with TJa.Window.Text;       use TJa.Window.Text;
 with Ada.Sequential_IO;
 
 package Score_Handling is
@@ -13,6 +15,8 @@ package Score_Handling is
    procedure Put_Highscore(X     : in Integer;
 			   Y     : in Integer;
 			   Total : in Integer);
-   
+   procedure Put_Score( Socket : in Socket_Type;
+			Game   : in Game_Data);
+   procedure Get_Score(Socket : in Socket_Type);
    
 end Score_Handling;
