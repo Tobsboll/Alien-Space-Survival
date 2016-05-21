7919733784b50bfe9012abf89ac1e22fe34082d0
syte89 <ericandersson2@hotmail.com>
2016-05-21 22:13:43 +0200

    Update print_handling.adb

--- print_handling.adb
+++ print_handling.adb
@@ -64,7 +64,7 @@ package body Print_Handling is
       Set_Colours(Red, Bottom_BG_Colour);
       Set_Bold_Mode(On);
       
-      if Loop_Counter < 299 then
+      if Loop_Counter < 300 then
 	 Goto_XY(GameBorder_X, GameBorder_Y+World_Y_Length+2);
 	 Put("Start");
 	 
@@ -74,22 +74,23 @@ package body Print_Handling is
 	 end loop;
 	 
 	 for I in 1..3 loop
-	    Goto_XY((GameBorder_X+7+Integer(Float(Loop_Counter)/3.0)), GameBorder_Y+World_Y_Length+I);
+	    Goto_XY((GameBorder_X+7+Integer(Float(Loop_Counter)/3.5)), GameBorder_Y+World_Y_Length+I);
 	    Put("▬▶");
 	 end loop;
 	 
 	 for I in 1..3 loop
-	    Goto_XY(GameBorder_X+(World_X_Length-2), GameBorder_Y+World_Y_Length+I);
+	    Goto_XY(GameBorder_X+World_X_Length-16, GameBorder_Y+World_Y_Length+I);
 	    Put('|');
 	 end loop;
 	 
-	 Goto_XY(GameBorder_X+(World_X_Length), Gameborder_Y+World_Y_Length+2);
+	 Goto_XY(GameBorder_X+World_X_Length-14, Gameborder_Y+World_Y_Length+2);
 	 Put("New Level");
       else
-	 Goto_XY(GameBorder_X+(World_X_Length/2), GameBorder_Y+World_Y_Length+2);
+	 Goto_XY(GameBorder_X+(World_X_Length/2)-7, GameBorder_Y+World_Y_Length+2);
 	 Put("Level ");
 	 Put(Level, Width => 2);
       end if;
+      
       -----------------------------------------------------------------
       --| Weapons Status
       -----------------------------------------------------------------
@@ -149,19 +150,23 @@ package body Print_Handling is
 	    Put("Diagonal-Laser");
 	 end if;
       end if;
+      
+      
+      -----------------------------------------------------------------
+      --| COOLDOWN BOX
       -----------------------------------------------------------------
       
       Set_Bold_Mode(Off); -- Måste stänga av annars är färgen Dark_Grey på boxen under
-      Put_Double_Line_Box(Gameborder_X+World_X_Length+2, Gameborder_Y+Highscore_Window_Height+10, Highscore_Window_Width-2, 2, Game_Wall_Background, Black);
+      Put_Double_Line_Box(Gameborder_X+World_X_Length+1, Gameborder_Y+Highscore_Window_Height+15, Highscore_Window_Width-1, 2, Game_Wall_Background, Black);
 
       
       Set_Foreground_Colour(Red);
-      Goto_XY(Gameborder_X+World_X_Length+3, Gameborder_Y+Highscore_Window_Height+11);
+      Goto_XY(Gameborder_X+World_X_Length+2, Gameborder_Y+Highscore_Window_Height+16);
       for I in 1 .. Cooldown loop
 	 Put("▌");
       end loop;
       Set_Foreground_Colour(Black);
-      Goto_XY(Gameborder_X+World_X_Length+13, Gameborder_Y+Highscore_Window_Height+10);
+      Goto_XY(Gameborder_X+World_X_Length+11, Gameborder_Y+Highscore_Window_Height+15);
       Put("COOLDOWN");
       Set_Foreground_Colour(White);
       Set_Bold_Mode(On);
@@ -170,15 +175,15 @@ package body Print_Handling is
       --| Power TIME BOX
       -----------------------------------------------------------------
       Set_Bold_Mode(Off); -- Måste stänga av annars är färgen Dark_Grey på boxen under
-      Put_Double_Line_Box(Gameborder_X+World_X_Length+2, Gameborder_Y+Highscore_Window_Height+13, Highscore_Window_Width-2, 2, Game_Wall_Background, Black);
+      Put_Double_Line_Box(Gameborder_X+World_X_Length+1, Gameborder_Y+Highscore_Window_Height+18, Highscore_Window_Width-1, 2, Game_Wall_Background, Black);
       
-      Goto_XY(Gameborder_X+World_X_Length+3, Gameborder_Y+Highscore_Window_Height+14);
+      Goto_XY(Gameborder_X+World_X_Length+2, Gameborder_Y+Highscore_Window_Height+19);
       Set_Foreground_Colour(Red);
       for I in 1 .. Power_Time loop
 	 Put("▌");
       end loop;
       Set_Foreground_Colour(Black);
-      Goto_XY(Gameborder_X+World_X_Length+12, Gameborder_Y+Highscore_Window_Height+13);
+      Goto_XY(Gameborder_X+World_X_Length+10, Gameborder_Y+Highscore_Window_Height+18);
       Put("POWER LEVEL");
       Set_Foreground_Colour(White);
       Set_Bold_Mode(On);
@@ -188,27 +193,30 @@ package body Print_Handling is
       --| Powerup info box
       -----------------------------------------------------------------
       Set_Foreground_Colour(White);
-      Put_Line_Space_Box(Gameborder_X+World_X_Length+2, Gameborder_Y+Highscore_Window_Height+16, Highscore_Window_Width-2, 10, Game_Wall_Background, White);
-      Goto_XY(Gameborder_X+World_X_Length+11, Gameborder_Y+Highscore_Window_Height+17); 
+            Put_Box(Gameborder_X+World_X_Length+1, Gameborder_Y+Highscore_Window_Height+22, Highscore_Window_Width-1, 10, Game_Wall_Background, White);
+      
+      
+      
+      Goto_XY(Gameborder_X+World_X_Length+11, Gameborder_Y+Highscore_Window_Height+23); 
       Set_Underlined_Mode(On);
       Put("Powerup Info");
       Set_Underlined_Mode(Off);
       
-      Goto_XY(Gameborder_X+World_X_Length+7, Gameborder_Y+Highscore_Window_Height+18); 
-      Put("(♥) - +5 Health");
-      Goto_XY(Gameborder_X+World_X_Length+7, Gameborder_Y+Highscore_Window_Height+19); 
-      Put("(m) - Missile Ammo");
-      Goto_XY(Gameborder_X+World_X_Length+7, Gameborder_Y+Highscore_Window_Height+20); 
-      Put("(M) - Super Missile");
-      Goto_XY(Gameborder_X+World_X_Length+7, Gameborder_Y+Highscore_Window_Height+21); 
-      Put("(o) - Laser upgrade");
-      Goto_XY(Gameborder_X+World_X_Length+7, Gameborder_Y+Highscore_Window_Height+22); 
-      Put("(X) - Hitech Laser");
-      Goto_XY(Gameborder_X+World_X_Length+7, Gameborder_Y+Highscore_Window_Height+23); 
-      Put("(3) - Tri-Laser");
       Goto_XY(Gameborder_X+World_X_Length+7, Gameborder_Y+Highscore_Window_Height+24); 
-      Put("(D) - Diagonal-laser");
+      Put("(♥) - +5 Health");
       Goto_XY(Gameborder_X+World_X_Length+7, Gameborder_Y+Highscore_Window_Height+25); 
+      Put("(m) - Missile Ammo");
+      Goto_XY(Gameborder_X+World_X_Length+7, Gameborder_Y+Highscore_Window_Height+26); 
+      Put("(M) - Super Missile");
+      Goto_XY(Gameborder_X+World_X_Length+7, Gameborder_Y+Highscore_Window_Height+27); 
+      Put("(o) - Laser upgrade");
+      Goto_XY(Gameborder_X+World_X_Length+7, Gameborder_Y+Highscore_Window_Height+28); 
+      Put("(X) - Hitech Laser");
+      Goto_XY(Gameborder_X+World_X_Length+7, Gameborder_Y+Highscore_Window_Height+29);
+      Put("(3) - Tri-Laser");
+      Goto_XY(Gameborder_X+World_X_Length+7, Gameborder_Y+Highscore_Window_Height+30); 
+      Put("(D) - Diagonal-laser");
+      Goto_XY(Gameborder_X+World_X_Length+7, Gameborder_Y+Highscore_Window_Height+31); 
       Put("(N) - Nitro-shot");
       -----------------------------------------------------------------
       
