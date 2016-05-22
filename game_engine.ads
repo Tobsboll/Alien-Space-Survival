42333d18aea215d87c31a988f6fc69550af7a978
McKvack <you_tas@hotmail.com>
2016-05-22 04:29:53 +0200

    Update game_engine.ads

--- game_engine.ads
+++ game_engine.ads
@@ -27,8 +27,8 @@ with TJa.Sockets;            use TJa.Sockets;     --
 with Ada.Text_IO;            use Ada.Text_IO;     --
 with TJa.Keyboard;           use TJa.Keyboard;    --
 with Graphics;               use Graphics;        --
-with Ada.Numerics.Discrete_Random;
-----------------------------------------------------
+with Ada.Numerics.Discrete_Random;                --
+						  ----------------------------------------------------
 
 package Game_Engine is
    
@@ -36,9 +36,9 @@ package Game_Engine is
    package Powerup_random is
       new Ada.Numerics.Discrete_Random(Result_Subtype => One_To_Ten);
    use Powerup_Random;
-     
-   Gen       : Generator;
    
+   Gen       : Generator;
+
    --Procedure Set_Default_Values fastställer alla startvärden som behövs innan
    --spelet börjar
    procedure Set_Default_Values (Num_Players    : in Integer;
@@ -47,7 +47,9 @@ package Game_Engine is
 				 Players_Choice : out Players_Choice_Array;
 				 Level          : out Integer;
 				 Level_Cleared  : out Boolean;
-				 New_Level      : out Boolean);
+				 New_Level      : out Boolean;
+				 Wall_List      : in out Object_List
+				);
    
    --Shot_Movement uppdaterar skottens position på banan i rätt riktning.
    --Riktningen anges som attribut till Object_Type när man skapar ett skott
@@ -79,22 +81,22 @@ package Game_Engine is
 			   X1_Offset : in Integer := 0) return Boolean;
    
    --Overlapping_Y returnerar true om två intervall i y-led har samma värde
-     function Overlapping_Y (Y1, Y2 : in Integer;
-			     Y1_Length, Y2_Length : in Integer;
-			     Y1_Offset : in Integer := 0) return Boolean;
-     
-     --Overlapping_XY är en kombination av ovanstående och returnerar därför
-     --true om två fyrkanter överlappar varandra
-     function Overlapping_XY  (XY1, XY2 : in XY_Type;
-			       X1_Width, Y1_Length, X2_Width, Y2_Length : in Integer;
-			       X1_Offset : in Integer :=0;
-			       Y1_Offset : in Integer := 0
-			      ) return Boolean;
-     
-     --Ship_Overlapping är en funktion som ovan fast utformad för skeppets unika
-     --form
-     function Ship_Overlapping (X1,Y1, X2,Y2, X2_Width,Y2_Length: in Integer
-			   ) return Boolean;
+   function Overlapping_Y (Y1, Y2 : in Integer;
+			   Y1_Length, Y2_Length : in Integer;
+			   Y1_Offset : in Integer := 0) return Boolean;
+   
+   --Overlapping_XY är en kombination av ovanstående och returnerar därför
+   --true om två fyrkanter överlappar varandra
+   function Overlapping_XY  (XY1, XY2 : in XY_Type;
+			     X1_Width, Y1_Length, X2_Width, Y2_Length : in Integer;
+			     X1_Offset : in Integer :=0;
+			     Y1_Offset : in Integer := 0
+			    ) return Boolean;
+   
+   --Ship_Overlapping är en funktion som ovan fast utformad för skeppets unika
+   --form
+   function Ship_Overlapping (X1,Y1, X2,Y2, X2_Width,Y2_Length: in Integer
+			     ) return Boolean;
    
    -- Funktionen player_collide retunerar en boolean beroende på om spelarens grafiska
    -- koordinater krockar med andra objekts grafiska koordinater.
@@ -114,12 +116,14 @@ package Game_Engine is
 				      );
    
    function Shot_Collide (Shot, obj : in Object_List) return boolean;
-   procedure A_Shot_Collide_In_Object (Shot, Obj2   : in out Object_List;
-				       Game         : in out Game_Data;
-				       Powerup_List : in out Object_List);
-   procedure Shots_Collide_In_Objects (Obj1, Obj2   : in out Object_List;
-				       Game         : in out Game_Data;
-				       Powerup_List : in out Object_List);
+   procedure A_Shot_Collide_In_Object (Shot, Obj2 : in out Object_List;
+				       Game        : in out Game_Data;
+				       Powerup_List : in out Object_List
+				      );
+   procedure Shots_Collide_In_Objects (Obj1, Obj2 : in out Object_List;
+				       Game        : in out Game_Data;
+				       Powerup_List : in out Object_List
+				      );
    
    --BETA:
    --Players_Are_Dead ska returnera true om ALLA spelare är döda
@@ -128,8 +132,8 @@ package Game_Engine is
    
    --
    procedure Create_Wall ( L : in out Object_List;
-			     Ypos : in Integer);
-			     
+			   Xpos, Ypos : in Integer);
+   
    --------------------------------------------------
    -- TILLÄGG FRÅN TOBIAS:
    -- två funktioner som hanterar väntetid mellan händelser
@@ -153,26 +157,32 @@ package Game_Engine is
    procedure Revive_Player(PlayerNumber : in Integer;
 			   Player       : in out Player_Array);
    
+   procedure Spawn_Powerup(X, Y         : in Integer;
+			   Powerup_List : in out Object_List);
+   
    procedure Create_Explosion_Big (L : in out Object_List;
-					   X, Y : in Integer);
+				   X, Y : in Integer);
    
    procedure Create_Explosion_Medium (L : in out Object_List;
-				        X, Y : in Integer);
+				      X, Y : in Integer);
    procedure Create_Explosion_Small (L : in out Object_List;
-				      X , Y  : in Integer);
+				     X , Y  : in Integer);
    procedure Create_Ricochet (L             : in out Object_List;
-			       X , Y         : in Integer);
+			      X , Y         : in Integer);
    procedure Create_Nitro_Explosion (L : in out Object_List;
 				     X, Y    : in Integer);
    procedure Create_Hitech_Explosion ( L     : in out Object_List;
 				       X , Y : in Integer);
    procedure Create_Nuke (L : in out Object_List;
 			  X, Y : in Integer);
+   
+   procedure Detonate (L : in out Object_List;
+		       Expl : in out Object_List;
+		       XOffset : in Integer := 0;
+		       YOffset : in Integer := 0);
+   
    procedure Activate_Thrusters (L : in out Object_List;
 				 X, Y : in Integer);
    procedure Create_Side_Thrust ( L          : in out Object_List;
 				  X , Y      : in Integer);
-   
-   procedure Spawn_Powerup(X, Y         : in Integer;
-			   Powerup_List : in out Object_List);
 end Game_Engine;
